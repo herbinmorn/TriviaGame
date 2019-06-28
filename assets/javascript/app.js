60bd6f61d2deb03f4d5e54fb5901c777b3712f10
Xiaoquan Zhou <xiaoquan.zhou@email.edcc.edu>
2019-06-27 23:11:11 -0700

    need working on hide(),reset and 1st result count

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,10 +6,11 @@ var answer2 =['Sugar','Glucose','Magnesium','Stevia'];
 var answer3=['Carbohydrates','Lactose','Zinc','Maple Syrup'];
 var answer4=['Proteins','Maltose','Iron','Caramel'];
 var imageTitles= ['The correct answer is sugar.','The correct answer is glucose.','The correct answer is Mg, magnesium.','The correct answer is stevia.'];
-var images =['images/sugar.jpg','images/glucose.jpg','images/mg.jpg','images/stevia.jpg'];
+var images =['assets/images/sugar.jpg','assets/images/glucose.jpg','assets/images/mg.jpg','assets/images/stevia.jpg'];
 
+var timelimit = 30;
 var intervalId;
-var number =30;
+var number = timelimit;
 var i=0;
 function run(){
     clearInterval(intervalId);
@@ -21,7 +22,8 @@ function decrement(){
     $('#timer').html('<div>Time Remaining: ' + number +' Seconds</div>');
     if (number === 0){
         stop();
-        showAnswers(i);}
+        showAnswers(i);
+        unanswered++;}
     }
 
 
@@ -31,26 +33,28 @@ function stop(){
 }
 
 
-var showQuestions=function(i){
-
+var showQuestions=function(){
+    //$('#answer-canvas').hide();
+    //$('#summary-canvas').hide();
+    number = timelimit;
+    run();
     $("#questions").html("<button>"+questions[i]+"</button>");
-    $("#answer1").html("<button>"+answer1[i]+"</button>");
-    $("#answer2").html("<button>"+answer2[i]+"</button>");
-    $("#answer3").html("<button>"+answer3[i]+"</button>");
-    $("#answer4").html("<button>"+answer4[i]+"</button>");
-
-    setTimeout(function() {
-        showAnswers(i);
-    }, 3000);
+    $("#answer1").html("<button id=\"answer1\">"+answer1[i]+"</button>");
+    $("#answer2").html("<button id=\"answer2\">"+answer2[i]+"</button>");
+    $("#answer3").html("<button id=\"answer3\">"+answer3[i]+"</button>");
+    $("#answer4").html("<button id=\"answer4\">"+answer4[i]+"</button>");
+    $("#image-title").html("");
+    $("#image-holder").html("");
 }
 
 var correct = 0;
 var wrong =0;
 var unanswered =0;
 
-var showAnswers = function(i) {
+var showAnswers = function() {
+   // $('#question-canvas').hide();
+    //$('#summary-canvas').hide();
     
-   /* if(/*clickcorrect){$("#yesNo").html("<div>Correct!Congratulations!</div>");correct++;}else{$("#yesNo").html("<div>Nope!</div>");wrong++;}*/
     stop();
     $("#image-title").html("<div>"+imageTitles[i]+"</div>");
     $("#image-holder").html("<img src=" + images[i] + " width='400px'>");
@@ -70,22 +74,42 @@ var showAnswers = function(i) {
 
 
 var showRecord = function(){
+    //$('#question-canvas').hide();
+   // $('#answer-canvas').hide();
     $("#correct").html("Correct Answers:"+ correct);
     $("#wrong").html("Incorrect Answers:"+ wrong);
     $("#unanswered").html("Unanswered:"+ unanswered);
 
 }
 
-var startGame= function(){
-    showQuestions(0);
-    run();
+function yes(){
+    showAnswers();
+    $('#yesNo').html('<div>Correct! Congratulations!</div>');
+    correct = correct + 1;
+    
 }
 
+function no(){
+    showAnswers();
+    $('#yesNo').html('<div>Nope!</div>');
+    wrong = wrong + 1;
+    
+}
 
-
+var startGame= function(){
+    clearInterval(intervalId);
+    showQuestions();
+    
+}
 
 $(".start").click(startGame);
-$(".choices").click(function(){showAnswers(i);});
+//$(".choices").click(function(){showAnswers();});
+$('#answer2').click(yes);
+$('#answer1').click(no);
+$('#answer3').click(no);
+$('#answer4').click(no);
 
+//$('#start-canvas').show();
+//$('#question-canvas').hide();
 
 
